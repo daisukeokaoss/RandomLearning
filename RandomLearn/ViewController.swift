f8824446268ce316b1c603ef514d9bace14b623a
岡大輔 <daisukeokahassou@gmail.com>
2015-07-10 20:52:37 +0900

    Initial Commit

---
+++ RandomLearn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomLearn
+//
+//  Created by 岡大輔 on 2015/07/10.
+//  Copyright (c) 2015年 岡大輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
